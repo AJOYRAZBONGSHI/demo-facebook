50b5c2af7d6205d7f5a7e5dee001b8967d09fee5
AJOYRAZBONSHIe <razbongshiajoy111u@gmaile.com>
2022-08-30 20:48:18 +0600

    function anather way

--- js/function.js
+++ js/function.js
@@ -1,19 +1,44 @@
 // =/=/=/=/=/=/=/   like increase system /=/=/=/=/=/=/=/=/=/
 
+// -/-/-/-/-/-/-/-/-/-/- by getElementsByClassName /-/-/-/-/-/-/-/-/-/-
 function increaseLike(n) {
-    const likes = document.querySelectorAll('.like');
+    const likes = document.getElementsByClassName('like');
     const like = likes[n];
     let likeIncrease = + like.innerText;
     likeIncrease++;
     like.innerText = likeIncrease;
     like.style.color = 'red';
-    const likeIn = document.querySelectorAll('.liked');
+    const likeIn = document.getElementsByClassName('liked');
     const liked = likeIn[n];
     liked.style.color = 'green';
 }
+// -/-/-/-/-/-/-/-/-/-/- by querySelectorAll /-/-/-/-/-/-/-/-/-/-
+// function increaseLike(n) {
+//     const likes = document.querySelectorAll('.like');
+//     const like = likes[n];
+//     let likeIncrease = + like.innerText;
+//     likeIncrease++;
+//     like.innerText = likeIncrease;
+//     like.style.color = 'red';
+//     const likeIn = document.querySelectorAll('.liked');
+//     const liked = likeIn[n];
+//     liked.style.color = 'green';
+// }
 
 // /=/=/=/=/=/=/=/=/=/ comment publish system /=/=/=/=/=/=/=/=/=/=/=
 
+// -/-/-/-/-/-/-/-/-/-/- by getElementsByClassName /-/-/-/-/-/-/-/-/-/-
+// function commentPost(n) {
+//     const commentBoxes = document.getElementsByClassName('textarea');
+//     const comment = commentBoxes[n].value;
+//     const newComment = document.createElement('p');
+//     newComment.innerText = comment;
+//     const post = 'post' + n;
+//     document.getElementById(post).appendChild(newComment);
+//     commentBoxes[n].value = '';
+// }
+
+// -/-/-/-/-/-/-/-/-/-/- by querySelectorAll /-/-/-/-/-/-/-/-/-/-
 function commentPost(n) {
     const commentBoxes = document.querySelectorAll('.textarea');
     const comment = commentBoxes[n].value;
@@ -23,3 +48,4 @@ function commentPost(n) {
     document.getElementById(post).appendChild(newComment);
     commentBoxes[n].value = '';
 }
+
